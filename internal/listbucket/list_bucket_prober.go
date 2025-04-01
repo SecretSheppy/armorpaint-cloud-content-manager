c39a1468d074b83589bb87517835de66171eba0b
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2025-04-01 13:56:06 +0100

    feat: added list bucket prober

---
+++ internal/listbucket/list_bucket_prober.go
@@ -0,0 +1,36 @@
+package listbucket
+
+import "github.com/SecretSheppy/armorpaint-cloud-content-manager/internal/httpm"
+
+func ProbeOnce(URL string) (*ListBucketResult, error) {
+	raw, err := httpm.Download(URL)
+	if err != nil {
+		return &ListBucketResult{}, err
+	}
+
+	return NewListBucketResult(raw)
+}
+
+func ProbeAll(URL string) (*ListBucketResult, error) {
+	result, err := ProbeOnce(URL)
+	if err != nil {
+		return &ListBucketResult{}, err
+	}
+
+	if !result.IsTruncated {
+		return result, nil
+	}
+
+	markerURL, err := GetMarkerURL(URL, result.NextMarker)
+	if err != nil {
+		return &ListBucketResult{}, err
+	}
+
+	allResults, err := ProbeAll(markerURL)
+	if err != nil {
+		return &ListBucketResult{}, err
+	}
+
+	allResults.Contents = append(result.Contents, allResults.Contents...)
+	return allResults, nil
+}
